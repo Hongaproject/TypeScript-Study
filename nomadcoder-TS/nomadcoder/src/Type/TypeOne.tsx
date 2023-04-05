74e00833e004ea5c7202f3413116c910e07d917b
Hongaproject <tkwlscjq99@gmail.com>
2023-04-05 17:42:41 +0900

    1day Type

---
+++ nomadcoder-TS/nomadcoder/src/Type/TypeOne.tsx
@@ -0,0 +1,36 @@
+    let a : number = 1;
+    let b : string = "i1";
+    let c : boolean = true;
+
+    let a1 : number[] = [1]; //let a1 : number[] = [1];
+    let b1 : string[] = ["i1"];
+    let c1 : boolean[] = [true];
+
+    const player : {
+        name : string,
+        age? : number //age를 선택적으로 만들어줌.
+    } = {
+        name : "Hongga"
+    }
+    console.log(player);
+
+    type Age = number;
+    type Name = string;
+    type Player = {
+        name:string,
+        age?:number
+    }
+    // const hongga : Player = {
+    //     name: "hongga"
+    // }
+    // console.log(hongga);
+
+// function TypeOne (name:string) {
+//     return(
+//         name
+//     );
+// }
+
+const TypeOne = (name:string) : Player => ({name})
+
+
