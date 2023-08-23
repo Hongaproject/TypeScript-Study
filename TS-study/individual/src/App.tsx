6f3194c935270c225d362390b5d77c64c6854172
Hongaproject <tkwlscjq99@gmail.com>
2023-08-23 23:33:32 +0900

    typescript interface type 정리

--- TS-study/individual/src/App.tsx
+++ TS-study/individual/src/App.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
 import './App.css';
 
+interface itf1 { // 처음 생성
+  a: number;
+  b: string;
+}
+
+interface itf2 extends itf1 { // interface 확장 선언
+  c: string;
+}
+
+const face: itf2 = { 
+  a: 1,
+  b: 'b',
+  c: 'c'
+}
+
+interface itf {
+  a: number;
+}
+interface itf { // interface 중복선언 가능 
+  b: number;
+}
+const inter: itf = {
+  a: 1,
+  b: 2
+}
+
+
+type tp1 = {
+  a: number;
+  b: string;
+}
+
+type tp2 = tp1 & { // type 확장 선언 
+  c: string;
+}
+
+const tpc: tp2 = {
+  a: 1,
+  b: 'b',
+  c: 'c'
+}
+
+type tp = { // type 중복 선언 X 
+  a: number;
+}
+
+type tp = {
+  b: number;
+}
+
+
+
 function App() {
+
   return (
     <div>
       
