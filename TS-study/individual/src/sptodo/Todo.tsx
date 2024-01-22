b2497cb4cded018f99e8456d0bd91adbc5d3824f
Hongaproject <tkwlscjq99@gmail.com>
2024-01-22 20:32:44 +0900

    타입스크립트 투드리스트 제작 완성

--- TS-study/individual/src/sptodo/Todo.tsx
+++ TS-study/individual/src/sptodo/Todo.tsx
@@ -37,29 +37,100 @@ function Todo () {
         setTodoList(todoList.filter((item) => item.id !== id));
     }
 
-    console.log(onDelete);
+    const onUpdate = (newList: List): void => {
+        const newTodoList = todoList.map((item) => {
+            if (item.id === newList.id) {
+              return newList;
+            } else {
+              return item;
+            }
+        })
+        setTodoList(newTodoList);
+    }
     
     return(
         <div>
             TodoList!
             <div>
-                <form onSubmit={onSubmit}>
-                    <input type="text" placeholder="할 일을 입력하세요." onChange={onChange} value={text} />
-                    <button>등록</button>
-                </form>
-            </div>
-            <div>
+            <CreateTodo onChange={onChange} onSubmit={onSubmit} text={text}/>
                 {
-                    todoList.map((item, index) => (
-                        <div key={index}>
-                            <p>{item.title}</p>
-                            <div>
-                                <button onClick={() => onDelete}>삭제</button>
-                            </div>
+                    todoList.map((item) => (
+                        <div>
+                            <Todoitem key={item.id} id={item.id} title={item.title} onDelete={onDelete} onUpdate={onUpdate}/>
                         </div>
                     ))
                 }
             </div>
+        </div>
+    );
+}
+
+interface Item {
+    id: number;
+    title: string;
+    onDelete(id:number): void;
+    onUpdate(updateItem: List): void;
+}
+
+
+function Todoitem ({id, title, onDelete, onUpdate}: Item) {
+
+    const [isUpdate, setIsUpdate] = useState<boolean>(false);
+    const [updateTitle, setUpdateTitle] = useState<string>(title);
+
+    const updateOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUpdateTitle(e.target.value);
+    }
+
+    const updateOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const updateItem: List = {
+            id: id,
+            title: updateTitle
+        };
+        onUpdate(updateItem);
+        setIsUpdate(false);
+    }
+    
+    return(
+        <div>
+            {!isUpdate ? (
+                <div>
+                    {title}
+                    <div>
+                        <button onClick={() => setIsUpdate(true)}>수정</button>
+                        <button onClick={() => onDelete(id)}>삭제</button>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <form onSubmit={updateOnSubmit}>
+                        <input type="text" placeholder="수정 할 내용을 입력해주세요!" onChange={updateOnChange} value={updateTitle}/>
+                        <div>
+                            <button type="submit">확인</button>
+                            <button onClick={() => setIsUpdate(false)}>취소</button>
+                        </div>
+                    </form>
+                </div>
+            )
+            }
+        </div>
+    );
+}
+
+interface Create {
+    onChange(e: React.ChangeEvent<HTMLInputElement>): void;
+    onSubmit(e: React.FormEvent<HTMLFormElement>): void;
+    text: string;
+}
+
+function CreateTodo({onChange, onSubmit, text}: Create) {
+    return(
+        <div>
+            <form onSubmit={(e) => onSubmit(e)}>
+                <input type="text" placeholder="할 일을 입력해주세요!" onChange={(e) => onChange(e)} value={text} />
+                <button type="submit">등록</button>
+            </form>
         </div>
     );
 }
