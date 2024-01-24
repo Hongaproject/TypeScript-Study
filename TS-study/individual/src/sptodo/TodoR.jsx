e16beaa0837b92e534e688ca1ea925a7efde2312
Hongaproject <tkwlscjq99@gmail.com>
2024-01-24 20:54:29 +0900

    리액트 투두리스트 코드 설명

--- TS-study/individual/src/sptodo/TodoR.jsx
+++ TS-study/individual/src/sptodo/TodoR.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function TodoR () {
 
-    const [list, setList] = useState([
+    const [list, setList] = useState([ // 배열로 내용을 만들어서 map을 사용해서 내용을 보여지게 만듬.
         {
             id: 1,
             title: '할 일1',
@@ -12,20 +12,20 @@ function TodoR () {
             title: '할 일2',
         },
     ])
-    const [text, setText] = useState("");
+    const [text, setText] = useState(""); // input에 입력 할 때 value 값을 받기 위해 사용
 
     const onChange = (e) => { // input에 입력시 코드 실행을 시키기 위한 이벤트 핸들러 
         setText(e.target.value);
     }
 
-    const onSubmit = (e) => {
+    const onSubmit = (e) => { // 입력 한 내용을 저장 할 수 있게 도와줌.
         e.preventDefault();
-        const newList = {
+        const newList = { 
             id: Date.now(),
             title: text
         }
-        setList([...list, newList]);
-        setText("");
+        setList([...list, newList]); // spread 문법을 사용해서 list부분을 복사한 후, newList내용을 뒤에 붙힌다.
+        setText(""); // 텍스트를 작성하고 완료가 되었을 때 초기화를 시킴.
     }
     
     const onDelete = (id) => {
@@ -62,16 +62,16 @@ function TodoRItem ({list, title, onDelete, setList, id}) {
         setUpdateText(e.target.value);
     }
 
-    const onSubmit = (e) => {
+    const onSubmit = (e) => { 
         e.preventDefault();
-        const newUpdate = list.map((data) => {
-            if(data.id === id){
-                data.title = updateText
+        const newUpdate = list.map((data) => { // list 부분에 있는 내용을 map으로 나타내어줌.
+            if(data.id === id){ // list부분에 있는 id를 가져와서 id가 동일하다면
+                data.title = updateText // updateText값이 들어가게 해준다.
             }
-            return data
+            return data 
         });
 
-        setList(newUpdate);
+        setList(newUpdate); // 바뀐 부분을 setList에 저장해준다.
         setIsUpdate(false);
     }
 
